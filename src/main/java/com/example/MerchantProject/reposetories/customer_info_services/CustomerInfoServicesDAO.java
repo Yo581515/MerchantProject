e3b6fcfd614b70e38be32b579c9ce507c39897db
haako <haakon.mydland@gmail.com>
2022-09-28 16:15:48 +0200

    dssd

--- src/main/java/com/example/MerchantProject/reposetories/customer_info_services/CustomerInfoServicesDAO.java
+++ src/main/java/com/example/MerchantProject/reposetories/customer_info_services/CustomerInfoServicesDAO.java
@@ -1,6 +1,7 @@
 package com.example.MerchantProject.reposetories.customer_info_services;
 
+import com.example.MerchantProject.javaclasses.customer_info_services.CustomerInfoServices;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CustomerInfoServicesDAO extends JpaRepository<Student,Integer> {
+public interface CustomerInfoServicesDAO extends JpaRepository<CustomerInfoServices,Integer> {
 }
